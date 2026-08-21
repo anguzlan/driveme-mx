02750339e657fb5e7ae75dc4a8cfefc64145ddd6
Antonio Guzmán Landero <jlandero@Antonios-MacBook-Air.local>
2026-08-20 21:40:38 -0600

    Forzar IPv4 y puerto 6543 en URL directa

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,10 @@
 import { Pool } from 'pg';
 
-// Ponemos la URL directamente para ignorar Render y su caché
 export const pool = new Pool({
-  connectionString: 'postgresql://postgres.tahmobalaiyvshhxsltz:Megacable23%23@aws-0-us-west-2.pooler.supabase.com:5432/postgres',
+  // Nota el puerto 6543, es el correcto para el Pooler de Supabase
+  connectionString: 'postgresql://postgres.tahmobalaiyvshhxsltz:Megacable23%23@aws-0-us-west-2.pooler.supabase.com:6543/postgres',
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  family: 4 // ¡ESTO ES VITAL PARA BLOQUEAR LA RUTA IPv6 EN RENDER!
 });
