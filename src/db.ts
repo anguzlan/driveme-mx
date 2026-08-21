40e1c946fb51eabcc018d5c4192fa08e939c417e
Antonio Guzmán Landero <jlandero@Antonios-MacBook-Air.local>
2026-08-20 21:42:52 -0600

    Forzar guardado con puerto 6543 y family 4

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,9 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-  // Nota el puerto 6543, es el correcto para el Pooler de Supabase
   connectionString: 'postgresql://postgres.tahmobalaiyvshhxsltz:Megacable23%23@aws-0-us-west-2.pooler.supabase.com:6543/postgres',
   ssl: {
     rejectUnauthorized: false
   },
-  family: 4 // ¡ESTO ES VITAL PARA BLOQUEAR LA RUTA IPv6 EN RENDER!
+  family: 4
 });
