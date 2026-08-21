c98d17a3fa5ac6e4ba65cae8ba231adac141344a
Antonio Guzmán Landero <jlandero@Antonios-MacBook-Air.local>
2026-08-20 21:51:17 -0600

    Eliminar JS fantasma y forzar puerto 6543

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,12 @@
 import { Pool } from 'pg';
 
+console.log("🚩 ¡LEYENDO DB.TS CORRECTO! INICIANDO EN PUERTO 6543 🚩");
+
+// Borrar cualquier variable de entorno de Render que confunda a la base de datos
+delete process.env.PGHOST;
+delete process.env.PGPORT;
+delete process.env.DATABASE_URL;
+
 export const pool = new Pool({
   host: 'aws-0-us-west-2.pooler.supabase.com',
   port: 6543,
