3ded3aad050b9d86da012651133a831dc770658c
Antonio Guzmán Landero <jlandero@Antonios-MacBook-Air.local>
2026-08-20 20:43:53 -0600

    Actualizar db.ts con host explitico y family 4

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,9 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: 'db.tahmobalaiyvshhxsltz.supabase.co',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: process.env.DB_PASSWORD || 'Megacable23#', // O puedes usar process.env.DATABASE_URL si prefieres, pero esto fuerza IP y evita IPv6
   ssl: {
     rejectUnauthorized: false
   },
-  family: 4 // Esto fuerza a usar IPv4 y evita el error ENETUNREACH en Render
+  family: 4
 });
