26dd38b84a69571e511ff6a75184fefedf8bc1b9
Antonio Guzmán Landero <jlandero@Antonios-MacBook-Air.local>
2026-08-20 22:01:39 -0600

    Hardcodeando URL del Pooler para ignorar variables de Render

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,8 @@
 import { Pool } from 'pg';
-import dotenv from 'dotenv';
-import dns from 'node:dns';
-
-// 🚀 EL TRUCO MAESTRO: Obliga a Node.js (Render) a usar IPv4 en toda la aplicación
-dns.setDefaultResultOrder('ipv4first');
-
-dotenv.config();
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  // URL directa del Pooler con puerto 6543. ¡Ignoramos a Render por completo!
+  connectionString: 'postgresql://postgres.tahmobalaiyvshhxsltz:Megacable23%23@aws-0-us-west-2.pooler.supabase.com:6543/postgres',
   ssl: {
     rejectUnauthorized: false
   }
